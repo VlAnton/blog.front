5112689cf1a1f20663c2feb52750568dcd7fa3d0
VlAnton <antocha1616@gmail.com>
2024-11-28 18:46:42 +0500

    added components and views folders

---
+++ src/components/NavBar/NavBar.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import NavBarItem from '@/components/NavBar/NavBarTab.vue'
+import { ref } from 'vue'
+
+const tabs = [
+  {
+    id: 0,
+    title: 'Главная',
+    link: 'home',
+  },
+  {
+    id: 1,
+    title: 'Обо мне',
+    link: 'about',
+  },
+  {
+    id: 2,
+    title: 'Контакты',
+    link: 'contacts',
+  },
+]
+
+const currentTabId = ref(0)
+</script>
+
+<template>
+  <header :class="$style.header">
+    <nav-bar-item
+      v-for="tab in tabs"
+      :key="tab.id"
+      :active="tab.id === currentTabId"
+      :title="tab.title"
+      :link="tab.link"
+      @click="currentTabId = tab.id"
+    />
+  </header>
+</template>
+
+<style module>
+.header {
+  padding: 48px 24px;
+  background-color: #f1ebff;
+  display: flex;
+  gap: 32px;
+}
+</style>
---
+++ src/components/NavBar/NavBarTab.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import { defineProps, withDefaults } from 'vue'
+
+type HeaderProps = {
+  title: string
+  active: boolean
+  link: string
+}
+
+const props = withDefaults(defineProps<HeaderProps>(), {
+  title: '',
+  active: false,
+  link: '#',
+})
+</script>
+
+<template>
+  <router-link :to="link" :class="$style['header-tab']">
+    {{ title }}
+    <div
+      :class="{
+        [$style.underline]: true,
+        [$style['underline--active']]: props.active,
+      }"
+    ></div>
+  </router-link>
+</template>
+
+<style module>
+.header-tab {
+  color: #9264ff;
+  cursor: pointer;
+  text-decoration: none;
+  border-bottom: 1px solid transparent;
+  transition: border-color ease-out 0.3s;
+}
+
+.underline {
+  height: 1px;
+  width: 0;
+  background-color: #9264ff;
+  transition: all ease-out 0.2s;
+}
+
+.underline--active,
+.header-tab:hover > .underline {
+  width: 100%;
+  transition: all ease-in 0.2s;
+}
+</style>
